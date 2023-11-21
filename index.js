3875e5c3663b5a1b7d8cb79378e8e375fb38914e
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-21 19:48:05 +0600

    3 q. done

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.log(a); // undefined
+// var a = 5;
+
+// console.log(b); // ReferenceError: Cannot access 'b' before initialization
+// let b = 3;
+
+// console.log(2 + "2" - 1); // 21
+
